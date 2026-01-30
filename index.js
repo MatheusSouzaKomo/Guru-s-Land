eee0bbbd7cb6dcd9d056c3a2a5f868dcb165d8b7
dev-kauams <kauams.profissional@gmail.com>
2026-01-30 12:05:16 -0300

    commit: mais uns bag ai

--- index.js
+++ index.js
@@ -21,3 +21,25 @@ setInterval(() => {
     index = (index + 1 ) % totalSlides;
     atualizarSlide();
 }, 5000);
+
+
+function setupModal(buttonOpenId, modalId, buttonCloseId) {
+    const buttonOpen = document.getElementById(buttonOpenId);
+    const modal = document.getElementById(modalId);
+    const buttonClose = document.getElementById(buttonCloseId);
+
+    if(buttonOpen && modal && buttonClose) {
+        buttonOpen.addEventListener('click', () => modal.classList.add('active'));
+        buttonClose.addEventListener('click', () => modal.classList.remove('active'));
+        
+        // Fechar ao clicar fora da caixa branca
+        window.addEventListener('click', (event) => {
+            if (event.target === modal) {
+                modal.classList.remove('active');
+            }
+        });
+    }
+}
+
+setupModal('openMap', 'map', 'closeModal');
+setupModal('openFoodCourt', 'food-court', 'closeFoodCourt');
