04289ec7a56cd8f461c22c9f7b21646185192f81
MatheusSouzaKomo <matheus.snsbs03@gmail.com>
2026-01-30 09:49:10 -0300

    Commit images and classes update

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-const botao = document.getElementById('calltoaction');
-const botao2 = document.getElementById('btn-topo');
+let index = 0;
 
+const slides = document.querySelector(".slides");
+const totalSlides = document.querySelectorAll(".slide").length;
+
+function atualizarSlide() {
+    slides.style.transform = `translateX(-${index * 100}%)`;
+}
+
+document.querySelector(".next").addEventListener("click", () => {
+    index = (index + 1 ) % totalSlides;
+    atualizarSlide();
+});
+
+document.querySelector(".prev").addEventListener("click", () => {
+    index = (index - 1 + totalSlides) % totalSlides;
+    atualizarSlide();
+});
+
+setInterval(() => {
+    index = (index + 1 ) % totalSlides;
+    atualizarSlide();
+}, 5000);
